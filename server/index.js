37748ea5c2ecf89c1a0557741e140b2170e9fef4
careylzh <lzh.carey@gmail.com>
2021-04-15 20:19:24 +0800

    wrote basic structure for routing post/get reqs(to be filled with actual routes)

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// aws-connector code
+
+
+// connectors
+
+//Routing 
+app.get("/store_json", (req, res) => {
+//want to get all the relevant documents
+//  result = client.search{elastic search json string} 
+//client.search is an abstracted method from aws-elasticsearch-connector to access elasticsearch api directly
+  res.send(result);
+});
+
+
+app.get("/home", (req, res) => {
+  res.send(""); //see the data post to the actual endpoint above
+});
+
+// backend.com/blah
+app.post("/home", (req, res) => {
+  console.log(req.body);
+  res.sendStatus(200);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
