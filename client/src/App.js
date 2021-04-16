a8bf828545af5063b73402a77381881e6686fa75
careylzh <lzh.carey@gmail.com>
2021-04-16 09:40:49 +0800

    added empty searchpage(will contain SearchBar.js and SearchResultsTable)

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 
+import SearchPage from './Components/SearchPage'
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      
+      <SearchPage />
     </div>
   );
 }
---
+++ client/src/Components/SearchPage.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+
+const SearchPage = (props) => {
+
+ 
+  return (
+    <>
+      <h1>Is It Open?</h1>
+    </>
+   );
+}
+
+export default SearchPage
