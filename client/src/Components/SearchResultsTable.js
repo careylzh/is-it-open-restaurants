624e489491adf3c3e2b14ff5f96b485971378b1a
careylzh <lzh.carey@gmail.com>
2021-04-16 11:47:07 +0800

    component to generate html elements from retrieved json restaurants

--- client/src/Components/SearchResultsTable.js
+++ client/src/Components/SearchResultsTable.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
 const SearchResultsTable = ({restaurantList=[]}) => {
+  console.log("raw data passed to SearchResultsTable for generation of html elements", restaurantList);
   return (
     <>
     { restaurantList.map((data,index) => {
+      //turning on this debug message will flood console. only use to verify each datapoint retrieved from db is formatted properly as a json string.
+      //console.log("check if data is 1 datapoint in restaurantList received from SearchPage fns:", data); 
         if (data) {
           return (
-            <div key={data.name}>
-              <h1>{data.name}</h1>
+            <div key={data.id}>
+              <h3>{data.restaurantName}</h3>
+              <p>{data.openingHours}</p>
 	    </div>	
     	   )	
     	 }
