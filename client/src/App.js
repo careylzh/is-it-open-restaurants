41713be58b67e2eff5e31b10b39cf0075be14e29
careylzh <lzh.carey@gmail.com>
2021-04-18 09:53:42 +0800

    organised components into tabs

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
 import './App.css';
-
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
 import SearchPage from './Components/SearchPage'
 
 function App() {
   return (
     <div className="App">
+    
+    <Tabs>
+    <TabList>
+      <Tab>Search</Tab>
+      <Tab>Favourites</Tab>
+    </TabList>
+
+    <TabPanel>
+      <h2><SearchPage /></h2>
+    </TabPanel>
+    <TabPanel>
+      <h2>Lists of Favourites</h2> 
+    </TabPanel>
+    </Tabs>
       
-      <SearchPage />
     </div>
   );
 }
